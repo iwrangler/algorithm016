bdba54ff9519f4b0023a7c8b29308c63197e3bb3
MichaelWong6677 <MichaelWong6677@gmail.com>
2020-09-21 22:03:03 +0800

    两数之和习题

--- Week_01/geek_test.py
+++ Week_01/geek_test.py
@@ -1,3 +1,13 @@
-# coding:utf-8
-# this is a test file
-print("作业提交测试")
+# 题目：两数之和
+# author:MichaelWong6677
+# date:2020-09-21
+
+
+class Solution:
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for key, value in enumerate(nums):
+            if target - value in dic:  # 写之前判断，避免了重复元素的覆盖
+                return [dic[target - value], key]
+            dic[value] = key
